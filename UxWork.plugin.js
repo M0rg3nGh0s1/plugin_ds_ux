e26fd84ccb64ac6e406481292573cf3458d45853
MG <74490493+M0rg3nGh0s1@users.noreply.github.com>
2021-11-29 00:00:33 +0300

    Update UxWork.plugin.js

--- UxWork.plugin.js
+++ UxWork.plugin.js
@@ -1,1 +1,29 @@
+/**
+ * @name Support
+ * @displayName Support
+ * @source https://github.com/M0rg3nGh0s1/plugin_ds_ux/blob/main/UxWork.plugin.js
+ * @authorId 272771516872261632
+ */
 
+const config = {
+    info: {
+        name: "UxWork",
+        authors: [{
+            name: "MG",
+            discord_id: "272771516872261632",
+        }],
+        version: "0.0.1",
+        description: "Визуальный интерфейс для работы на ролях",
+        github: "https://github.com/M0rg3nGh0s1/plugin_ds_ux/blob/main/UxWork.plugin.js",
+        github_raw: "https://raw.githubusercontent.com/M0rg3nGh0s1/plugin_ds_ux/main/UxWork.plugin.js",
+
+    },
+    changelog: [{
+        title: "Plugin",
+        type: "fixed",
+        items: [
+            "Теперь работает"
+        ]
+    }],
+    defaultConfig: []
+};
